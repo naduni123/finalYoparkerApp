00b826e6f31b77074663d40744b916c4dbf9e48a
Heshan <heshan.imedha@gmail.com>
2021-07-29 10:29:33 +0530

    add card added

---
+++ app/src/main/java/com/example/finalapnlab/AddCard.java
@@ -0,0 +1,14 @@
+package com.example.finalapnlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_card);
+    }
+}
---
+++ app/src/main/java/com/example/finalapnlab/Reserve_Confirm.java
@@ -0,0 +1,14 @@
+package com.example.finalapnlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reserve_Confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reserve_confirm);
+    }
+}
